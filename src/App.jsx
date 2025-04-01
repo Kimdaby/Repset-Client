d18c237db5556a3a9f635625258324e5a911cc22
David Kim <davidhkim22@gmail.com>
2025-03-31 19:27:09 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      {/* <BrowserRouter>
+        <Router>
+          <Routes></Routes>
+        </Router>
+      </BrowserRouter> */}
       <Footer />
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import "./Header.scss";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__title">
+      <div className="header__logo">
         <h1>RepSet</h1>
+      </div>
+      <div className="header__nav">
+        <MenuIcon sx={{ fontSize: 32 }} />
       </div>
     </header>
   );
